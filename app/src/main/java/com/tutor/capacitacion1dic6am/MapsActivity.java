9fe09c2d4e751ad4484e1724d2823aaac13914d7
editorweb <jorge.r@msn.com>
2021-12-02 06:58:22 -0500

    test

---
+++ app/src/main/java/com/tutor/capacitacion1dic6am/MapsActivity.java
@@ -0,0 +1,37 @@
+package com.tutor.capacitacion1dic6am;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+    map = googleMap;
+        LatLng bucaramanga = new LatLng(7.1214,-73.1286);
+        map.addMarker(new MarkerOptions().position(bucaramanga).title("Bucaramanga"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(bucaramanga));
+
+
+    }
+}
